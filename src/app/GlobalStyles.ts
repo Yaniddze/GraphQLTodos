2cfa127bc68f4f333fef303a6146100e9a55ea7d
Yaniddze <FaceCrack1337@gmail.com>
2020-09-25 19:32:59 +0500

    :bug: Fix styles

--- src/app/GlobalStyles.ts
+++ src/app/GlobalStyles.ts
@@ -1,13 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  body, html, #root {
+  body, html {
     margin: 0;
     padding: 0;
     font-family: 'Roboto', sans-serif;
     background: ${(props): string => props.theme.colors.dim};
     color: ${(props): string => props.theme.colors.dark};
-    width: 100%;
-    height: 100%;
   }
 `;
